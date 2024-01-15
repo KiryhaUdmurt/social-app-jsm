fd20b87ea54b02ae97c265679bf8df4df5a5112e
Kirill Kalinin <kalinin1507.2012@gmail.com>
2024-01-15 22:23:23 +0400

    date-formatter

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,12 +1,44 @@
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
 
 type PostCardProps = {
-    
-}
+  post: Models.Document;
+};
 
-const PostCard = ({post}: PostCardProps) => {
+const PostCard = ({ post }: PostCardProps) => {
   return (
-    <div>PostCard</div>
-  )
-}
+    <div className="post-card">
+      <div className="flex-between">
+        <div className="flex itemscenter gap-3">
+          <Link to={`/profile/${post.creator.$id}`}>
+            <img
+              src={
+                post?.creator?.imageUrl ||
+                "/assets/icons/profile-placeholder.svg"
+              }
+              alt="creator"
+              className="rounded-full w-12 lg:h-12"
+            />
+          </Link>
 
-export default PostCard
+          <div className="flex flex-col">
+            <p className="base-medium lg:body-bald text-light-1">
+              {post.creator.name}
+            </p>
+            <div className="flex-center gap-2 text-light-3">
+              <p className="subtle-semibold lg:small-regular">
+                {post.$createdAt}
+              </p>
+              -
+              <p className="subtle-semibold lg:small-regular">
+                {post.location}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
