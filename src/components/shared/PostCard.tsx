98c6b7d67a68b564e5dc5dcda14caedc458ebfd1
Kirill Kalinin <kalinin1507.2012@gmail.com>
2024-01-18 13:09:31 +0400

    post details page done

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -15,7 +15,7 @@ const PostCard = ({ post }: PostCardProps) => {
   return (
     <div className="post-card">
       <div className="flex-between">
-        <div className="flex itemscenter gap-3">
+        <div className="flex items-center gap-3">
           <Link to={`/profile/${post.creator.$id}`}>
             <img
               src={
